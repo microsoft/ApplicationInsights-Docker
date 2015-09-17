1d6af183aa9343bc2c51349f35ea59e62b662e34
yonisha <yonisha@microsoft.com>
2015-09-17 10:31:22 +0300

    [License] Adding license headers

--- src/test/resources/test_state_event.py
+++ src/test/resources/test_state_event.py
@@ -1,3 +1,24 @@
-__author__ = 'yonisha'
+#
+# ApplicationInsights-Docker
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# MIT License
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the ""Software""), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+# __author__ = 'yonisha'
 
 print("{'name':'docker-container-state','ikey':'instrumentation_key','properties':{'Docker image':'ubuntu','docker-Created':'2015-08-06T09:01:55.3422261Z','docker-StartedAt':'2015-08-06T09:01:55.7843711Z','Docker container name':'/modest_jang0','docker-RestartCount':0,'docker-status':'start','Docker container id':'22e26598fcda9d72c6c7aa55c9ee5c91b52ff73c31202f163f1bcbf2668c2f18','Docker host':'galha-ubuntu'}}")
